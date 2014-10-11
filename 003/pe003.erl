1c80a9d344ee7bebce3c29c918f058a3670d0479
Yasuyuki Abe <Yasuyuki Abe@localhost>
2014-10-11 20:11:10 +0900

    problem 003 solved

---
+++ 003/pe003.erl
@@ -0,0 +1,20 @@
+-module(pe003).
+-import(primes, [primes/0]).
+-export([main/0]).
+
+solve(N, [Prime|_]) when Prime * Prime >= N -> N;
+solve(N, [Prime|PrimeLT]) ->
+  case N rem Prime of
+    0 -> solve(N div Prime, PrimeLT());
+    _ -> solve(N,           PrimeLT())
+  end.
+
+main() -> solve(600851475143, primes:primes()).
+
+
+% 1> cd("003").
+% 2> code:add_path("../primes").
+% 3> c(pe003).
+% 4> pe003:main().
+% 6857
+
