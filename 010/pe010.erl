503d066a453474248baa0bf8de1a00a7990fd9d0
Yasuyuki Abe <yasu@yasuabe.net>
2014-10-12 21:35:45 +0900

    improved drastically with new algorithms which uses only primes below sqrt(2M)

--- 010/pe010.erl
+++ 010/pe010.erl
@@ -1,14 +1,30 @@
 -module(pe010).
 -export([main/0]).
 -import(primes, [primes/0]).
+-import(lists, [any/2]).
 
-solve([Prime|_], Sum) when Prime > 2000000 -> Sum;
-solve([Prime|PrimeLT], Sum) ->
-  solve(PrimeLT(), Sum + Prime).
-  
-main() -> solve(primes(), 0).
+divisable_by_any(M, Primes) ->
+  any(fun(Prime)-> M rem Prime == 0 end, Primes).
 
-% 6> c("010/pe010.erl").
+diminish(_, _, 1, S) -> S;
+diminish(Primes, Prime, M, S) ->
+  Diminished = case not divisable_by_any(M, Primes) of
+    true  -> S - Prime * M;
+    false -> S
+  end,
+  diminish(Primes, Prime, M - 1, Diminished).
+
+solve(_, [Prime|_], S) when Prime * Prime > 2000000 -> S;
+solve(LesserPrimes, [Prime|PrimeLT], S) ->
+  S1 = diminish(LesserPrimes, Prime, 2000000 div Prime, S),
+  solve([Prime|LesserPrimes], PrimeLT(), S1).
+ 
+main() -> 
+  StartSum = 2 + 999999 * (3 + 1999999) div 2,
+  [P1|PT] = primes(),
+  solve([P1], PT(), 2 + StartSum).
+
+% 41> c("010/pe010.erl").
 % {ok,pe010}
-% 7> pe010:main().      
-% 142913828922
+% 42> pe010:main().      
+% 142913828924
